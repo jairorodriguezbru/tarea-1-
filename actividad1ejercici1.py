d2b1c1190e7e1720771dc880765f5c02606f15a8
DeliRojas2612 <89883027+DeliRojas2612@users.noreply.github.com>
2022-03-03 22:30:53 -0500

    agregado ejercicio uno como rama del segundo
    deli

---
+++ actividad1ejercici1.py
@@ -0,0 +1,21 @@
+#calcular el area de un triangulo
+from unittest import result
+
+
+#base=int (input("ingrese la base del triangulo "))
+#alruta=int (input("ingrese la altura del triangulo "))
+#calculararea= (base * alruta)/ 2
+
+
+
+def main():
+
+    base=int (input("ingrese la base del triangulo "))
+    altura=int (input("ingrese la altura del triangulo "))
+    area= (base * altura) / 2
+    
+    print("el area de el triangulo es : " ,area,"cm²:" ) 
+
+
+if __name__=="__main__": 
+    main()   
