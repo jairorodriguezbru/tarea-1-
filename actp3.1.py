1369c0ff956cec4caafbec07def580dc91735dde
PablocHernandezm <90220712+PablocHernandezm@users.noreply.github.com>
2022-03-03 22:29:33 -0500

    parte 1 del ejercicio 3

---
+++ actp3.1.py
@@ -0,0 +1,15 @@
+def main():
+    a = int(input("digite el valor a"))
+    b = int(input("digite el valor b"))
+    c = int(input("digite el valor c"))
+    
+    x1 = funcioncuadratica(a,b,c)
+    x2 = funcioncuadratica(a,b,c)
+    print(f"Los valores de la funcion cuadratica son: x1= {x1} y x2= {x2}")
+
+
+
+
+    if ___name___ == "___main___":
+        main()
+        
